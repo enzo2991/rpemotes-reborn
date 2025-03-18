7001880936ee3e6ec8c061b9d692c5749a852133
Enzo Galantino <Enzo2991@live.fr>
2025-03-18 21:56:02 +0100

    Feat : add NetEvent if player is not dead

--- client/frameworks/ox.lua
+++ client/frameworks/ox.lua
@@ -1,5 +1,5 @@
 if Config.Framework ~= 'ox' then return end
-
+PlayerData = nil
 local framework = 'ox_core'
 local state = GetResourceState(framework)
 
@@ -7,3 +7,64 @@ if state == 'missing' or state == "unknown" then
     -- Framework can't be used if it's missing or unknown
     return
 end
+
+RegisterNetEvent('animations:client:PlayEmote', function(args)
+    if not LocalPlayer.state.isDead then
+        EmoteCommandStart(source, args)
+    end
+end)
+
+if Config.Keybinding then
+    RegisterNetEvent('animations:client:BindEmote', function(args)
+        if not LocalPlayer.state.isDead then
+            EmoteBindStart(nil, args)
+        end
+    end)
+
+    RegisterNetEvent('animations:client:EmoteBinds', function(_)
+        if not LocalPlayer.state.isDead then
+            EmoteBindsStart()
+        end
+    end)
+
+    RegisterNetEvent('animations:client:EmoteDelete', function(args)
+        if not LocalPlayer.state.isDead then
+            DeleteEmote(args)
+        end
+    end)
+end
+
+RegisterNetEvent('animations:client:EmoteMenu', function()
+    if not LocalPlayer.state.isDead then
+        OpenEmoteMenu()
+    end
+end)
+
+RegisterNetEvent('animations:client:ListEmotes', function()
+    if not LocalPlayer.state.isDead then
+        EmotesOnCommand()
+    end
+end)
+
+RegisterNetEvent('animations:client:Walk', function(args)
+    if not LocalPlayer.state.isDead then
+        WalkCommandStart(args)
+    end
+end)
+
+RegisterNetEvent('animations:client:ListWalks', function()
+    if not LocalPlayer.state.isDead then
+        WalksOnCommand()
+    end
+end)
+
+CanDoEmote = true
+RegisterNetEvent('animations:ToggleCanDoAnims', function(bool)
+    CanDoEmote = bool
+end)
+
+RegisterNetEvent('animations:client:EmoteCommandStart', function(args)
+    if CanDoEmote then
+        EmoteCommandStart(source, args)
+    end
+end)
--- server/frameworks/ox.lua
+++ server/frameworks/ox.lua
@@ -4,7 +4,35 @@ local framework = 'ox_core'
 local state = GetResourceState(framework)
 
 if state == 'missing' or state == "unknown" then
-    -- Framework can't be used if it's missing or unknown
     return
+end
+
+local Ox = require "@ox_core.lib.init"
+
+RegisterCommand('e',function(source, args, rawCommand)
+    local player = Ox.GetPlayer(source)
+    player.emit('animations:client:PlayEmote', args)
+end,false)
+
+RegisterCommand('emote',function(source, args, rawCommand)
+    local player = Ox.GetPlayer(source)
+    player.emit('animations:client:PlayEmote', args)
+end,false)
+
+if Config.Keybinding then
+    RegisterCommand('emotebind',function(source, args, rawCommand)
+        local player = Ox.GetPlayer(source)
+        player.emit('animations:client:BindEmote', args)
+    end,false)
+
+    RegisterCommand('emotebinds',function(source, args, rawCommand)
+        local player = Ox.GetPlayer(source)
+        player.emit('animations:client:EmoteBinds')
+    end,false)
+
+    RegisterCommand('emotedelete',function(source, args, rawCommand)
+        local player = Ox.GetPlayer(source)
+        player.emit('animations:client:EmoteDelete', args)
+    end,false)
 end
 
