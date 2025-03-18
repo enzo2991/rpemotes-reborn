a039f1b55bd5800259dec2b0daca0850372e5e67
Enzo Galantino <Enzo2991@live.fr>
2025-03-18 20:40:04 +0100

    Feat : add file for ox

---
+++ server/frameworks/ox.lua
@@ -0,0 +1,10 @@
+if Config.Framework ~= 'ox' then return end
+
+local framework = 'ox_core'
+local state = GetResourceState(framework)
+
+if state == 'missing' or state == "unknown" then
+    -- Framework can't be used if it's missing or unknown
+    return
+end
+
